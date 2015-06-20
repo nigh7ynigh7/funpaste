b006e430df7f43cc3ea4d05684c9968cc361e52b
nigh7ynigh7 <psxworld101@gmail.com>
2015-06-20 20:27:44 -0300

    time for some tests!

---
+++ app/tests/models/pasteTests.js
@@ -0,0 +1,133 @@
+var pastesModel = require('../../models/pastes');
+var mongoose = require('mongoose');
+var should = require('should');
+
+module.exports = function() {
+    describe('user model', function() {
+        describe('validation', function() {
+            var validations = [
+                'normal',
+                'c',
+                'cs',
+                'cpp',
+                'java',
+                'javascript',
+                'json',
+                'ruby',
+                'vb',
+                'html',
+                'css',
+                'stylus',
+                'jade',
+                'python',
+                'sql',
+                'md',
+                'x86asm'
+            ];
+
+            validations.forEach(function(input) {
+                it('should return true when passed : ' + input, function(done) {
+                    var result = pastesModel.validatePostLang(input);
+
+                    var expected = true;
+
+                    result.should.be.true;
+
+                    done();
+                });
+            });
+
+
+
+            it('should return false when passed \'xyzebf\'', function(done) {
+                var param = 'xyzebf';
+
+                var result = pastesModel.validatePostLang(param);
+
+                result.should.be.false;
+
+                done();
+            });
+        });
+
+
+        describe('paste persistance', function() {
+            var paste = {};
+            var persistedPaste;
+
+            before(function(done){
+                paste = {
+                    upTime : 10,
+                    content : 'Test content, will be removed in 10  minutes.',
+                    language : 'normal',
+                    postDate : new Date()
+                };
+                done();
+            });
+
+
+            it('should return a full paste when passed a temp paste', function(done){
+                var result;
+                pastesModel.savePaste(paste, function(err, rsp){
+                    if (err) {
+                        result = err;
+                    }else {
+                        result = rsp;
+                    }
+                    result.should.not.be.a.String;
+                    result.should.be.ok;
+                    result.should.be.an.Object;
+                    result.should.have.property('_id');
+                    result.should.have.property('removalDate');
+                    done();
+                });
+            });
+        });
+
+        describe('getting pastes', function(){
+
+            it('should return null or error if 0 is passed as', function(done){
+                var result;
+                pastesModel.getPaste(0, function(err, res){
+                    if (err) {
+                        result = err;
+                    }else {
+                        result = err;
+                    }
+                    result.should.not.be.ok;
+                    result.should.not.be.a.String;
+                    done();
+                });
+            });
+
+            it('should return an object if a correct id is passed', function(done){
+                var paste = {
+                    upTime : 10,
+                    content : 'Test get content, will be removed in 10  minutes.',
+                    language : 'normal',
+                    postDate : new Date()
+                };
+
+                pastesModel.savePaste(paste, function(err, result){
+                    if (err) {
+                        should.fail('Something went down', err);
+                        done();
+                    }else {
+                        pastesModel.getPaste(result._id , function(err, found){
+                            if (err) {
+                                should.fail('Something went down', err);
+                                done();
+                            }else {
+                                found.should.be.ok;
+                                found.should.be.Object;
+                                found.should.have.property('_id');
+                                found.should.have.property('removalDate');
+                                done();
+                            }
+                        });
+                    }
+                });
+            });
+        });
+    });
+};
--- app/tests/test.js
+++ app/tests/test.js
@@ -1,38 +1,5 @@
-process.env.NODE_ENV = process.env.NODE_ENV || 'test';
+var env = process.env.NODE_ENV = process.env.NODE_ENV || 'test';
 
 
-var assert = require('assert');
-var pastesModel = require('../models/pastes');
-var mongoose = require('mongoose');
-
-
-describe('user model', function(){
-    describe('validation', function(){
-        it('should return true when passed \'normal\'', function(done){
-            var param = 'normal';
-
-            var result = pastesModel.validatePostLang(param);
-
-            var expected = true;
-
-            assert.equal(result, expected);
-
-            done();
-        });
-
-        it('should return false when passed \'xyzebf\'', function(done){
-            var param = 'xyzebf';
-
-            var result = pastesModel.validatePostLang(param);
-
-            var expected = false;
-
-            assert.equal(result, expected);
-
-            done();
-        });
-    });
-
-
-
-});
+//test the paste models
+require('./models/pasteTests')();
