7e2d2240794fb0ce5db72903a95abf077b33e306
nigh7ynigh7 <psxworld101@gmail.com>
2015-06-23 08:39:30 -0300

    Update pasteTests.js

--- app/tests/models/pasteTests.js
+++ app/tests/models/pasteTests.js
@@ -148,7 +148,7 @@ module.exports = function() {
             });
         });
 
-        describe.skip('Removing pastes', function(){
+        describe('Removing pastes', function(){
             it('should delete some pastes within one minute of posting', function(done){
                     pastesModel.timedKill(function(isError, msg){
                     if (isError) {
